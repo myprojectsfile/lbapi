ed9b9beadc56f753ca7b1badf7c7c2a059bb3461
myprojectsfile <ahmadi80@gmail.com>
2017-04-21 18:19:00 +0430

    initial git

---
+++ server/boot/roles.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(app) {
+
+//   var User = app.models.User;
+//   var Role = app.models.Role;
+//   var RoleMapping = app.models.RoleMapping;
+
+//  User.create([
+//     {username: 'admin', email: 'admin@khod.com', password: 'admin'}
+//   ], function(err, users) {
+//     if (err) console.log(err);
+
+//     //create the admin role
+//     Role.create({
+//       name: 'administrator'
+//     }, function(err, role) {
+//       if (err) console.log(err);
+
+//       //make bob an admin
+//       role.principals.create({
+//         principalType: RoleMapping.USER,
+//         principalId: users[0].id
+//       }, function(err, principal) {
+//         console.log(err);
+//       });
+//     });
+//   });
+
+
+};
